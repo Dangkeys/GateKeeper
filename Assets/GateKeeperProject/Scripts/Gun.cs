ce8f485689480bab42497e9a2f7cf9391843b6f1
billza555 <nphisu@gmail.com>
2026-02-16 20:27:33 +0700

    feat: create gun script

---
+++ Assets/GateKeeperProject/Scripts/Gun.cs
@@ -0,0 +1,128 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private InputActionReference shootInput;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GunData data;
+    [SerializeField] private float recoilReturnSpeed = 8f;
+    [SerializeField] private TMP_Text ammoText;  
+    private float recoilTarget;
+    private float recoilCurrent; 
+    private float nextFireTime;
+    private int currentAmmo;
+
+    void Start()
+    {
+        data = Instantiate(data);
+        currentAmmo = data.magazineSize;
+        UpdateAmmo();
+    }
+
+    void Update()
+    {
+        bool isShooting = data.isAutoGun ? shootInput.action.IsPressed() : shootInput.action.WasPressedThisFrame();
+        UpdateRecoil(isShooting);
+
+        if (isShooting)
+        {
+            TryShoot();
+        }
+        if(currentAmmo == 0)
+        {
+            currentAmmo = data.magazineSize;
+            UpdateAmmo();
+        }
+    }
+
+    private void TryShoot()
+    {
+
+        if (Time.time < nextFireTime) return;
+        if (currentAmmo <= 0) return;
+
+        nextFireTime = Time.time + 1f / data.fireRate;
+
+        if (data.freeAmmoPercent <= 0f ||
+            Random.Range(0f, 100f) > data.freeAmmoPercent)
+        {
+            currentAmmo--;
+            UpdateAmmo();
+        }
+
+        for (int i = 0; i < data.pelletCount; i++)
+        {
+            Vector3 spreadDirection = GetSpreadDirection();
+
+            Ray ray = new Ray(firePoint.position, spreadDirection);
+
+            Debug.DrawRay(ray.origin, ray.direction * data.range, Color.red, 1f);
+
+            RaycastHit[] hits = Physics.RaycastAll(ray, data.range);
+            System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+
+            int penetrationCount = 0;
+            float currentDamage = data.flatDamage;
+
+            foreach (var hit in hits)
+            {
+                currentDamage *= 0.7f;
+                penetrationCount++;
+
+                if (penetrationCount >= data.penetration)
+                    break;
+            }
+        }
+
+        ApplyRecoil();
+    }
+
+    private Vector3 GetSpreadDirection()
+    {
+        float randomX = Random.Range(-data.spreadAngle, data.spreadAngle);
+        float randomY = Random.Range(-data.spreadAngle, data.spreadAngle);
+
+        Quaternion spreadRotation = Quaternion.Euler(randomX, randomY, 0f);
+
+        return spreadRotation * firePoint.forward;
+    }
+
+    private void UpdateRecoil(bool isShooting)
+    {
+        recoilCurrent = Mathf.Lerp(
+            recoilCurrent,
+            recoilTarget,
+            15f * Time.deltaTime
+        );
+
+        if (!isShooting)
+        {
+            recoilTarget = Mathf.Lerp(
+                recoilTarget,
+                0f,
+                recoilReturnSpeed * Time.deltaTime
+            );
+        }
+
+        transform.localRotation = Quaternion.Euler(-recoilCurrent, 0f, 0f);
+    }
+
+    private void ApplyRecoil()
+    {
+        float finalRecoil = data.recoil - data.recoilReduction;
+
+        recoilTarget += finalRecoil;
+
+        if(recoilTarget > data.maximumRecoil)
+        {
+            recoilTarget = data.maximumRecoil;
+        }
+    }
+
+    private void UpdateAmmo()
+    {
+        ammoText.text = currentAmmo.ToString() + " / " + data.magazineSize.ToString();
+    }
+}
---
+++ Assets/GateKeeperProject/Scripts/GunData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "VR/Weapon Data")]
+public class GunData : ScriptableObject
+{
+    [Header("Type")]
+    public bool isAutoGun = true;
+    [Header("Damage")]
+    public float flatDamage = 20f;
+    public float headshotMultiplier = 2f;
+    public float range = 100f;
+    public float penetration = 1f;
+
+    [Header("Fire Settings")]
+    public float fireRate = 5f; 
+    public float recoil = 2f;
+    public float recoilReduction = 0.5f;
+    public float maximumRecoil = 10f;
+
+    [Header("Ammo")]
+    public int magazineSize = 30;
+    public float reloadTime = 2f;
+    [Range(0,100)]
+    public float freeAmmoPercent = 0f;
+
+    [Header("Bullet")]
+    public float bulletSize = 0.1f;
+
+    [Header("Pellet")]
+    public float pelletCount = 1f;
+    public float spreadAngle = 5f;    
+}
