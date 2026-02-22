5c216a36dd52eeb2a0cb3f4324f5343378a47af1
billza555 <nphisu@gmail.com>
2026-02-22 13:10:52 +0700

    feat: create sprint script

---
+++ Assets/GateKeeperProject/Scripts/Sprint.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+
+public class Sprint : MonoBehaviour
+{
+    [SerializeField] private DynamicMoveProvider dynamicMoveProvider;
+    [SerializeField] private float initSpeed = 2.5f;
+    [SerializeField] private float multiplySpeed = 2f;
+    [SerializeField] private float sprintDuration = 10f;
+    [SerializeField] private InputActionReference sprintInput;
+    [SerializeField] private Scrollbar sprintScrollBar;
+    private float currentSprintDuration;
+    private bool isSprint;
+
+    void Start()
+    {
+        dynamicMoveProvider.moveSpeed = initSpeed;
+        currentSprintDuration = sprintDuration;
+        UpdateSprintScrollBar();
+    }
+
+    void OnEnable()
+    {
+        sprintInput.action.started += TrySprint;
+        sprintInput.action.canceled += TryNotSprint;
+    }
+
+    void OnDisable()
+    {
+        sprintInput.action.started -= TrySprint;
+        sprintInput.action.canceled -= TryNotSprint;
+    }
+
+    void Update()
+    {
+        if(isSprint)
+        {
+            if(currentSprintDuration > 0f)
+            {
+                currentSprintDuration -= Time.deltaTime;
+                UpdateSprintScrollBar();
+            }
+            else
+            {
+                isSprint = false;
+                currentSprintDuration = 0f;
+                dynamicMoveProvider.moveSpeed = initSpeed;
+                UpdateSprintScrollBar();
+            }
+        }
+        else
+        {
+            if(currentSprintDuration < sprintDuration)
+            {
+                currentSprintDuration += Time.deltaTime;
+                UpdateSprintScrollBar();
+            }
+        }
+    }
+
+    private void TrySprint(InputAction.CallbackContext context)
+    {
+        if(currentSprintDuration > 0f)
+        {
+            isSprint = true;
+            dynamicMoveProvider.moveSpeed = initSpeed * multiplySpeed;
+        }
+    }
+
+    private void TryNotSprint(InputAction.CallbackContext context)
+    {
+        isSprint = false;
+        dynamicMoveProvider.moveSpeed = initSpeed;
+    }
+
+    private void UpdateSprintScrollBar()
+    {
+        sprintScrollBar.size = currentSprintDuration / sprintDuration;
+    }
+
+}
