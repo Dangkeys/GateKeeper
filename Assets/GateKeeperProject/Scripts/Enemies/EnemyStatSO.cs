26b95d9cb7893c89668b665af0dd3f36bfb7915b
Trin Khunnean <coppertrin@gmail.com>
2026-03-20 22:51:17 +0700

    feat: add AttackFeedbackPrefab to enemy scriptable objects

--- Assets/GateKeeperProject/Scripts/Enemies/EnemyStatSO.cs
+++ Assets/GateKeeperProject/Scripts/Enemies/EnemyStatSO.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Serialization;
+using MoreMountains.Feedbacks;
 
 [CreateAssetMenu(fileName = "EnemyStatSO", menuName = "Scriptable Objects/EnemyStatSO")]
 public class EnemyStatSO : ScriptableObject
@@ -37,6 +38,9 @@ public class EnemyStatSO : ScriptableObject
     [field: SerializeField] public float AttackRange { get; private set; } = 2f;
     [field: SerializeField] public float AttackCooldown { get; private set; } = 1.5f;
      [field: SerializeField] public AgentType Type { get; private set; } = AgentType.Humanoid;
+
+    [Header("Feedbacks")]
+    [field: SerializeField] public MMF_Player AttackFeedbackPrefab { get; private set; }
 
     [Header("Roguelike / Rewards")]
     [field: SerializeField] public int scoreValue { get; private set; } = 100;
